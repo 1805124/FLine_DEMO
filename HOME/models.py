42e6e8bce80a62d190816daed46691775995a9c2
1805124 <43692914+1805124@users.noreply.github.com>
2022-04-21 23:24:05 +0530

    dashboard update
    updated dashboard + home notification management model

--- HOME/models.py
+++ HOME/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class notification_manage(models.Model):
+    sender = models.CharField(max_length=255)
+    f_type = models.CharField(max_length=255)
+    receiever = models.CharField(max_length=255)
+    r_type = models.CharField(max_length=255)
+    message = models.CharField(max_length=255)
+    time = models.DateTimeField()
+
